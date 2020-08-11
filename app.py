e8babc5acd7d17b9d8123b576d38da497be98962
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-11 21:27:51 +0100

    Add loop for scraping tapolgy events and extracting information

--- app.py
+++ app.py
@@ -1,36 +1,19 @@
 from bs4 import BeautifulSoup
-html_doc = """<!DOCTYPE html >
-<html lang = "en" >
-   <head >
-        <meta charset = "UTF-8" / >
-        <meta name = "viewport" content = "width=device-width, initial-scale=1.0" / >
-        <meta http-equiv = "X-UA-Compatible" content = "ie=edge" / >
-        <title > My Webpage < /title >
-    </head >
-    <body >
-        <div id = "section-1">
-            <h3 data-hello = "hi">Hello</h3>
-            <img src = "https://source.unsplash.com/200x200/?nature,water" />
-            <p >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-               culpa cumque velit aperiam officia molestias maiores qui
-                officiis incidunt. Omnis vitae eveniet reprehenderit excepturi
-                officiis quod, eum natus voluptatem nihil fugit necessitatibus
-                dolorum quae accusamus aliquid enim fuga dicta beatae!
-            </p >
-        </div >
-        <div id = "section-2">
-            <ul class = "items">
-                <li class = "item"><a href="#">Item 1</a></li>
-                <li class = "item"><a href="#">Item 2</a></li>
-                <li class = "item"><a href="#">Item 3</a></li>
-                <li class = "item"><a href="#">Item 4</a></li>
-                <li class = "item"><a href="#">Item 5</a></li>
-            </ul >
-        </div >
-    </body >
-</html >"""
+import requests
+from csv import writer
 
-soup = BeautifulSoup(html_doc, 'html.parser')
+response = requests.get('https://www.tapology.com/fightcenter')
 
-print(soup.title)
+soup = BeautifulSoup(response.text, 'html.parser')
+events = soup.findAll(class_='fcListing')
+
+with open("events.csv", "w") as csv_file:
+    csv_writer = writer(csv_file)
+
+for event in events:
+    event_data = []
+    event_formatted = event.get_text().split("\n")
+    for item in event_formatted:
+        if len(item.strip()) > 1:
+            event_data.append(item)
+    event = (' '.join(event_data) + '\n')
