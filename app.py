94545cb621c3862ff46e0ab5eb3560d30242a9a9
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-11 21:34:39 +0100

    Add functionality for writing events to csv

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ events = soup.findAll(class_='fcListing')
 
 with open("events.csv", "w") as csv_file:
     csv_writer = writer(csv_file)
-
-for event in events:
-    event_data = []
-    event_formatted = event.get_text().split("\n")
-    for item in event_formatted:
-        if len(item.strip()) > 1:
-            event_data.append(item)
-    event = (' '.join(event_data) + '\n')
+    headers = ["event"]
+    csv_writer.writerow(headers)
+    for event in events:
+        event_data = []
+        event_formatted = event.get_text().split("\n")
+        for item in event_formatted:
+            if len(item.strip()) > 1:
+                event_data.append(item)
+        event = (' '.join(event_data) + '\n')
+        csv_writer.writerow([event])
